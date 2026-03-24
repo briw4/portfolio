511264866e545e3a5791f41fae1ccdda64aac67a
marwabaani <marwabaani@gmail.com>
2026-03-24 03:03:29 +0100

    feat(layout): add navigation and configure global smooth scroll behavior

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,33 @@
 
 export default function Home(){
   return (
-    <main>
-      <section> 
-        <h1>Maroia Baani</h1>
-        <p>Welcome to my portfolio!</p>
+    <main className="scroll-smooth">
+      <section 
+        id="home" 
+        className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto"> 
+        <h1 className="text-4xl font-bold">Maroia Baani</h1>
+        <p className="mt-4 text-lg">Cybersecurity and networking student</p>
       </section>
-      <section> 
-        <h1>About Me</h1>
-        <p>I am a cybersecurity and networking student.</p>
+      <section 
+        id="about"
+        className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto"> 
+        <h2 className="text-3xl font-semibold">About Me</h2>
+        <p className="mt-4">I am a cybersecurity and networking student.</p>
       </section>
-      <section> 
-        <h1>Projects</h1>
-        <p>i will add a projects cards here after I complete them.</p>
+      <section 
+        id="projects"
+        className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto"> 
+        <h2 className="text-3xl font-semibold">Projects</h2>
+        <p className="mt-4">i will add a projects cards here after I complete them.</p>
       </section>
-      <section>
-        <h1>Contact</h1>
-        <p>You can reach me at
-          <a href="mailto:marwabaani@gmail.com">marwabaani@gmail.com</a>
-          or connect with me on 
-          <a href="https://www.linkedin.com/in/maroia-baani-896498244">LinkedIn</a>.
+      <section 
+        id="contact"
+        className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto">
+        <h2 className="text-3xl font-semibold">Contact</h2>
+        <p className="mt-4">You can reach me at{" "}
+          <a href="mailto:marwabaani@gmail.com" className="text-blue-500 hover:underline">marwabaani@gmail.com</a>
+          {" "}or connect with me on {" "}
+          <a href="https://www.linkedin.com/in/maroia-baani-896498244" className="text-blue-500 hover:underline">LinkedIn</a>.
         </p>
       </section>
     </main>
