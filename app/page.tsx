e1894cfb575db518857c3a32d57baf3432519f01
marwabaani <marwabaani@gmail.com>
2026-03-24 02:29:58 +0100

    feat(portfolio): create basic layout with hero, about, projects, and contact sections

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+
+export default function Home(){
+  return (
+    <main>
+      <section> 
+        <h1>Maroia Baani</h1>
+        <p>Welcome to my portfolio!</p>
+      </section>
+      <section> 
+        <h1>About Me</h1>
+        <p>I am a cybersecurity and networking student.</p>
+      </section>
+      <section> 
+        <h1>Projects</h1>
+        <p>i will add a projects cards here after I complete them.</p>
+      </section>
+      <section>
+        <h1>Contact</h1>
+        <p>You can reach me at
+          <a href="mailto:marwabaani@gmail.com">marwabaani@gmail.com</a>
+          or connect with me on 
+          <a href="https://www.linkedin.com/in/maroia-baani-896498244">LinkedIn</a>.
+        </p>
+      </section>
+    </main>
+  )
+}
