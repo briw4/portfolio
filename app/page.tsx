d45746aae3156aa33e9bedb7ed952635c20b98f6
marwabaani <marwabaani@gmail.com>
2026-03-24 12:42:56 +0100

    feat(home): add profile image section

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,41 @@
 export default function Home(){
   return (
     <main className="scroll-smooth">
+
       <section 
         id="home" 
         className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto"> 
-        <h1 className="text-4xl font-bold">Maroia Baani</h1>
-        <p className="mt-4 text-lg">Cybersecurity and networking student</p>
+        <div className="grid md:grid-cols-2 gap-10 items-center w-full">
+          <div className="flex flex-col gap-4">
+        <h1 className="text-5xl font-bold">Maroia Baani</h1>
+        <p className="text-xl">Cybersecurity and networking student</p>
+        </div>
+        </div>
+        <div className="flex justify-center md:justify-end">
+          <div className="w-64 h-64 rounded-full overflow-hidden border-4 border-gray-300 shadow-lg">
+             <img
+              src="/profile.jpg"
+              alt="Profile Picture"
+              className="w-full h-full object-cover"
+            />
+          </div>  
+        </div>
       </section>
+
       <section 
         id="about"
         className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto"> 
         <h2 className="text-3xl font-semibold">About Me</h2>
         <p className="mt-4">I am a cybersecurity and networking student.</p>
       </section>
+
       <section 
         id="projects"
         className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto"> 
         <h2 className="text-3xl font-semibold">Projects</h2>
         <p className="mt-4">i will add a projects cards here after I complete them.</p>
       </section>
+
       <section 
         id="contact"
         className="min-h-screen flex flex-col justify-center px-6 max-w-4xl mx-auto">
